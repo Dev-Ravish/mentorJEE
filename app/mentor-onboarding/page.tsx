3f6bc3e6d2b7c10ee62dd9d4de46f04c4cd5dc4f
dev-ravish <iamravish61@gmail.com>
2025-05-22 13:32:12 +0530

    Implement mentor onboarding flow and add Prisma setup

--- app/mentor-onboarding/page.tsx
+++ app/mentor-onboarding/page.tsx
@@ -1,13 +1,61 @@
 'use client'
 
-import {  SignIn, SignUp, useUser } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import {  SignedIn, SignedOut, SignIn, SignInButton, SignOutButton, SignUp, SignUpButton, useUser } from '@clerk/nextjs'
 
 export default function MentorOnboarding() {
     const { user } = useUser()
+    console.log(user)
 
-    if (!user) return <div className='flex flex-col items-center justify-center min-h-svh'>
-        <SignUp />
-    </div>
+    function isValidMentor() {
+        const email = user?.emailAddresses[0]?.emailAddress || '';
+        if (email.endsWith('@gmail.com')) {
+            return false;
+        } else{
+            return true;
+        }
+    }
 
-    return <div>Welcome!</div>
+
+    return (
+        <div>
+            <SignedOut>
+               <div>
+                    <div className='flex flex-col items-center justify-center min-h-screen'>
+                        <h1 className='text-2xl font-bold mb-4'>Welcome to MentorJee!</h1>
+                        <p className='mb-4'>Please sign in to continue.</p>
+                        <SignInButton mode="modal" >
+                            <Button className='bg-red-600 hover:bg-red-500'>Sign In</Button>
+                        </SignInButton>
+                        <p className='mt-4'>Don&apos;t have an account? <SignUpButton mode="modal"><Button>Sign Up</Button></SignUpButton></p>
+                    </div>
+                        
+               </div>
+            </SignedOut>
+            <SignedIn>
+               { isValidMentor() ? (
+                    <div>
+                        <div className='flex flex-col items-center justify-center min-h-screen'>
+                            <h1 className='text-2xl font-bold mb-4'>Welcome to MentorJee!</h1>
+                            <p className='mb-4'>We are excited to have you on board as a mentor.</p>
+                            <p className='mb-4'>Please fill out the form below to complete your onboarding process.</p>
+                            <SignOutButton >
+                                <Button className='bg-red-600 hover:bg-red-500 '>Sign Out</Button>
+                            </SignOutButton>
+                            {/* Add your onboarding form here */}
+                        </div>
+                    </div>
+                ) : (
+                    <div className='flex flex-col items-center justify-center min-h-screen'>
+                        <h1 className='text-2xl font-bold mb-4'>Sorry {user?.firstName}! you cannot be onboarded as our mentor. Please try logging In using college email id.</h1>
+                        <SignOutButton >
+                            <Button className='bg-red-600 hover:bg-red-500'>Sign Out</Button>
+                        </SignOutButton>
+                    </div>
+                )}
+            </SignedIn>
+        </div>
+
+
+    )
 }
--- app/mentors/page.tsx
+++ app/mentors/page.tsx
@@ -4,20 +4,20 @@ import { mentors } from '@/lib/mentorData';
 import ProfileCard from '@/components/profileCard';
 
 export default function MentorsPage() {
-  return (
-    <div className="min-h-screen px-6 py-10 bg-gray-50">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
-          Meet Our Mentors
-        </h1>
+    return (
+        <div className="min-h-screen px-6 py-10 bg-gray-50">
+        <div className="max-w-7xl mx-auto">
+            <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
+            Meet Our Mentors
+            </h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {mentors.map((mentor, index) => (
-            <ProfileCard key={index} mentor={mentor} />
-          ))}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {mentors.map((mentor, index) => (
+                <ProfileCard key={index} mentor={mentor} />
+            ))}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
